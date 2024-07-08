9c3043927bcf60b0785f8a816072d83501389eb1
Innocent Peter <otakhorpeter@gmail.com>
2024-07-08 05:16:03 +0100

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-import "./globals.css";
 import { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
 import Sidebar from "@/components/navigation/Sidebar";
